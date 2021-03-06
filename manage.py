fe97a688cbeac41601657752624c515c70ff8d2b
Dominik Wolf <dmnkwolf@googlemail.com>
2021-03-06 12:33:35 +0100

    added rofi configuration

--- manage.py
+++ manage.py
@@ -6,11 +6,23 @@ CONFIG_FILES = [
         r".config/i3",
         r".config/i3status",
         r".config/termite",
-        r".Xresources"
+        r".Xresources",
+        r".config/rofi"
         ]
 
 
-for config in CONFIG_FILES:
-    path = pathlib.Path.home() / config
-    target_path = pathlib.Path("./")
-    subprocess.run(["cp", "-r", path, target_path])
+def get_files():
+    for config in CONFIG_FILES:
+        path = pathlib.Path.home() / config
+        target_path = pathlib.Path("./")
+        subprocess.run(["cp", "-r", path, target_path])
+
+
+def deploy():
+    for config in CONFIG_FILES:
+        path = pathlib.Path("./") / config
+        target_path = pathlib.Path.home()
+        subprocess.run(["cp", "-r", path, target_path])
+
+
+get_files()
